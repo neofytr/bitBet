2ec2605052fc725afc133857e5b3d52620d21145
neofytr <imagination12344321.com@gmail.com>
2025-05-22 19:49:40 +0530

    run

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const ADMIN_CONFIG = {
 };
 
 const SERVER_CONFIG = {
-  host: "https://dca6-13-48-194-145.ngrok-free.app",
+  host: "https://f769-13-48-194-145.ngrok-free.app",
   port: "", // no port needed
   protocol: "https",
   get baseUrl() {
